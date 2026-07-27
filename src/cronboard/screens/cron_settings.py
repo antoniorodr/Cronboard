539806b1f319c9799b57992e8ff029430ae97560
Antonio Rodriguez <102554647+antoniorodr@users.noreply.github.com>
2026-07-27 12:02:42 +0200

    feat(CronSettings): create new settings widget

---
+++ src/cronboard/screens/cron_settings.py
@@ -0,0 +1,45 @@
+from textual import on
+from textual.app import ComposeResult
+from textual.binding import Binding
+from textual.containers import Grid
+from textual.widget import Widget
+from textual.widgets import Input, Label, MaskedInput, Select
+
+
+class CronSettings(Widget):
+    """Widget showing a list of the servers added by the user.
+
+    Attributes:
+        servers: Dict with the all the servers.
+        current_ssh_client: Paramiko SSH client for remote operations.
+        current_cron_table: CronTable with the cronjobs for the selected server.
+        current_server_name: Selected server name.
+    """
+
+    BINDINGS = [
+        Binding("J", "jump", "Switch Panel"),
+    ]
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def compose(self) -> ComposeResult:
+        """Builds the modal UI: CronTree with the servers and a Label."""
+
+        yield Label("Telegram notification")
+        yield Grid(
+            MaskedInput("Enter your Telegram token", id="telegram-token"),
+            Input("Enter your Telegram chat ID", id="telegram-chat-id"),
+            id="settings-grid",
+        )
+
+    @on(Select.Changed)
+    def select_changed(self, event: Select.Changed) -> None:
+        self.title = str(event.value)
+
+    def action_jump(self) -> None:
+        """Jumps to the settings options."""
+
+        settings_container: MaskedInput = self.query_one("#telegram-token", MaskedInput)
+        if settings_container:
+            settings_container.focus()
